07c2a68e9af6287db8b219ef1db841311e63af29
destinied <destiniediaz@mail.usf.edu>
2018-02-27 21:47:41 -0500

    Comment
    Un Comment

--- AdafruitDHT.py
+++ AdafruitDHT.py
@@ -4,7 +4,6 @@ import sys
 import Adafruit_DHT
 
 
-# Parse command line parameters.
 sensor_args = { '11': Adafruit_DHT.DHT11,
                 '22': Adafruit_DHT.DHT22,
                 '2302': Adafruit_DHT.AM2302 }
@@ -16,17 +15,8 @@ else:
     print('example: sudo ./Adafruit_DHT.py 2302 4 - Read from an AM2302 connected to GPIO #4')
     sys.exit(1)
 
-# Try to grab a sensor reading.  Use the read_retry method which will retry up
-# to 15 times to get a sensor reading (waiting 2 seconds between each retry).
 humidity, temperature = Adafruit_DHT.read_retry(sensor, pin)
 
-# Un-comment the line below to convert the temperature to Fahrenheit.
-# temperature = temperature * 9/5.0 + 32
-
-# Note that sometimes you won't get a reading and
-# the results will be null (because Linux can't
-# guarantee the timing of calls to read the sensor).
-# If this happens try again!
 if humidity is not None and temperature is not None:
     temp = (temperature * 1.8) + 32
     print('Temp={0:0.1f}*  Humidity={1:0.1f}%'.format(temp, humidity))
